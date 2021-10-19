af44df5fc81d829f03bab4583b6624fbaf6f95f5
JoseDeJesusMendozaBecerril <pepemendoza1311@gmail.com>
2021-10-19 14:54:09 -0500

    Final Project

--- Mamalona.java
+++ Mamalona.java
@@ -21,6 +21,7 @@ public class Mamalona {
     
     
     intp.interpret(direccion);
+    
   }
 
 
--- TinyParserGUI.java
+++ TinyParserGUI.java
@@ -5,17 +5,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.*;
 import javax.swing.JFileChooser;
+import javax.swing.text.BadLocationException;
 
 class TinyParserGUI extends JFrame implements ActionListener {
 	
 	static JButton btn_selectFile;
 	static JButton btn_run;
+	static JTextArea textArea;
+	static JTextArea screen;
 
+	static String direccion;
 	// create a frame
 	static JFrame f;
 
 	// create a textfield
 	static JTextField l;
+
+	
 
 	// store operator and operands
 	String inputString;
@@ -45,23 +51,28 @@ class TinyParserGUI extends JFrame implements ActionListener {
 
 		//Etiquetas de titulo
 		JLabel etiquetaVacia = new JLabel("                                                                                                                  ");
-		JLabel etiquetaTituloTextArea = new JLabel("Escribe tu código aquí:");
+		JLabel etiquetaTituloTextArea = new JLabel("Archivo");
 		etiquetaTituloTextArea.setForeground(Color.BLACK);
 		JLabel etiquetaTituloScreen = new JLabel("Resultado:");
 		etiquetaTituloScreen.setForeground(Color.BLACK);
 
 		// create a textArea
 		//este es el area de texto donde se escribe el código
-		JTextArea textArea = new JTextArea(10, 45);
+		textArea = new JTextArea(10, 45);
 		textArea.setEditable(true);
 		//Este otro no es editable por el usuario 
 		//y sera la pantalla donde se musetra la salida del código
-		JTextArea screen = new JTextArea(10, 45);
+		screen = new JTextArea(10, 45);
 		textArea.setEditable(false);
-
-
-
+		screen.setEditable(false);
 		
+
+		PrintStream printStream = new PrintStream(new CustomOutputStream(textArea));
+	    System.setOut(printStream);
+        System.setErr(printStream);
+
+		 
+        
 
 		// create a panel
 		JPanel p = new JPanel();
@@ -80,7 +91,7 @@ class TinyParserGUI extends JFrame implements ActionListener {
         //Agregamos el area de texto
         
 		p.add(etiquetaVacia);
-		p.add(etiquetaTituloTextArea);
+		//p.add(etiquetaTituloTextArea);
 		p.add(screen);
 		p.add(etiquetaTituloScreen);
 		p.add(textArea);
@@ -94,26 +105,76 @@ class TinyParserGUI extends JFrame implements ActionListener {
 	}
 
 	public void abrirFileChooser(ActionEvent e) {
-		/*
-		JFileChooser j = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-		// invoke the showsSaveDialog function to show the save dialog
-		int r = j.showSaveDialog(null);
-		// if the user selects a file
-		if (r == JFileChooser.APPROVE_OPTION){
-			// set the label to the path of the selected file
-			l.setText(j.getSelectedFile().getAbsolutePath());
-		}//*/
-		System.out.println("ABRIR VENTANA EMERGENTE");
+		
+		//System.out.println("ABRIR VENTANA EMERGENTE");
+
+		/*JALAR ARCHIVO DESDE MI DIRECTORIO*/
+
+    	File archivo;
+    	JFileChooser seleccionar;
+    	seleccionar = new JFileChooser();
+    	seleccionar.showOpenDialog(null);
+    	archivo = seleccionar.getSelectedFile();
+    
+    	//--LEER EL ARCHIVO PARA MANDAR A INTERPRETE
+    	direccion = seleccionar.getCurrentDirectory()+"/"+archivo.getName().toString();
+    
+		//--PEGAR TEXTO
+		String texto = leerArchivo(direccion);
+		screen.setText(texto);
+
+		//--LIMPIA TEXT AREA
+		try {
+			textArea.getDocument().remove(0,textArea.getDocument().getLength());
+			//standardOut.println("Text area cleared");
+		} catch (BadLocationException ex) {
+			ex.printStackTrace();
+		}
+
 	}
 
-	public void actionPerformed(ActionEvent e) {
+	public void ejecutar(ActionEvent e) throws Exception{
+		//System.out.println("ABRIR VENTANA EJECUTAR");
+		Interpreter intp = new Interpreter();
+		intp.interpret(direccion);
+
+
+
+	}
+
+	public void actionPerformed(ActionEvent e)  {
 		String s = e.getActionCommand();
 		// string is being input
 		// if the value is a number
 		if (s == "Select file") {
 			abrirFileChooser(e);
 		}else{
-			//Run presssed
+			try{
+				ejecutar(e);
+			}catch(Exception ex){
+				System.err.println("Error en el parse");
+			}
+			
 		}
+	}
+
+	public String leerArchivo(String direccion){
+		String texto = "";
+		try{
+			BufferedReader bf = new BufferedReader(new FileReader(direccion));
+			String temp = "";
+			String bfRead;
+
+			while((bfRead = bf.readLine()) != null){
+				temp = temp + bfRead + "\n";
+			}
+
+			texto = temp;
+
+		}catch(Exception e){
+			System.err.println("Error al leer el archivo");
+		}
+
+		return texto;
 	}
 }
