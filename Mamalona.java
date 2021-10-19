1f38fb11b2cf4f82d1b1d284c83397159be04afa
JoseDeJesusMendozaBecerril <pepemendoza1311@gmail.com>
2021-10-19 13:27:11 -0500

    Version consola

--- Mamalona.java
+++ Mamalona.java
@@ -1,8 +1,27 @@
+
+import java.io.File;
+
+
+import javax.swing.JFileChooser;
+
 public class Mamalona {
   
   public static void main(String[] args) throws Exception{
     Interpreter intp = new Interpreter();
-    String str = args[0];
-    intp.interpret(str);
+    
+    //--JALAR ARCHIVO DESDE MI DIRECTORIO --
+    File archivo;
+    JFileChooser seleccionar;
+    seleccionar = new JFileChooser();
+    seleccionar.showOpenDialog(null);
+    archivo = seleccionar.getSelectedFile();
+    
+    //--LEER EL ARCHIVO PARA MANDAR A INTERPRETE
+    String direccion = seleccionar.getCurrentDirectory()+"/"+archivo.getName().toString();
+    
+    
+    intp.interpret(direccion);
   }
+
+
 }
